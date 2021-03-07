f6ce5d4402ffeebbac028b23bd6984173ddfbb37
toantranbao <80157204+toantranbao@users.noreply.github.com>
2021-03-07 18:34:41 +0700

    The First commit

---
+++ src/main/java/mypack/HomeAPI.java
@@ -0,0 +1,16 @@
+package mypack;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeAPI {
+
+   @GetMapping(value = "/api/test")
+    public ResponseEntity<String> testSpringBoot() {
+        return ResponseEntity.ok("Success");
+    }
+}
