7ce6f1cec5820657d104ee54c9688bf48792e5be
quan12yt <57515987+quan12yt@users.noreply.github.com>
2021-03-07 19:15:27 +0700

    employee created

--- src/main/java/mypack/service/EmployeeService.java
+++ src/main/java/mypack/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package mypack.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mypack.entity.Employee;
 import mypack.repository.EmployeeRepository;
 
 @Service
@@ -11,5 +14,8 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	public List<Employee> getEmployees(){
+		return employeeRepository.findAll();
+	}
 	
 }
