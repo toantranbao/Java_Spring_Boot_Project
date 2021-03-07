f0d5795015d36a067e1766a2e7a60809a2284717
toantranbao <drivervaccine@gmail.com>
2021-03-07 18:57:51 +0700

    Designed the functions for developer to implement version 1.0

--- src/main/java/mypack/HomeAPI.java
+++ src/main/java/mypack/HomeAPI.java
@@ -13,4 +13,34 @@ public class HomeAPI {
     public ResponseEntity<String> testSpringBoot() {
         return ResponseEntity.ok("Success");
     }
+   
+   /*
+    * This function is using to ....
+    * Developed by Vuong
+    * Date: 
+    * */
+   @GetMapping(value = "/api/read/employees")
+   public ResponseEntity<String> r_employees() {
+       return ResponseEntity.ok("Success");
+   }
+   
+   /*
+    * This function is using to ....
+    * Developed by Quan
+    * Date: 
+    * */
+   @GetMapping(value = "/api/write/employee")
+   public ResponseEntity<String> w_employee() {
+       return ResponseEntity.ok("Success");
+   }
+   
+   /*
+    * This function is using to ....
+    * Developed by Len
+    * Date: 
+    * */
+   @GetMapping(value = "/api/read/employeeByName")
+   public ResponseEntity<String> r_employeeByName() {
+       return ResponseEntity.ok("Success");
+   }
 }
